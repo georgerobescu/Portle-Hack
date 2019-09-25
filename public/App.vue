485a96dab1b12593c119c3ce7b87d8c7eea53394
Timur Badretdinov <destinerx@gmail.com>
2019-09-25 15:14:17 +0300

    Replace bottom border with box shadow for header

--- public/App.vue
+++ public/App.vue
@@ -63,7 +63,7 @@ header {
 	padding: 0px 16px 0px 16px;
 	display: flex;
 	justify-content: space-between;
-	border-bottom: 1px solid #d3d3d3;
+	box-shadow: 0px 1px 4px -2px rgba(0,0,0,0.75);
 }
 
 footer {
