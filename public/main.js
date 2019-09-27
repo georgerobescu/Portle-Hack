eb8dcd7c82002878b4aa11a6a292b9887fc0f1b8
Timur Badretdinov <destinerx@gmail.com>
2019-09-27 21:09:23 +0300

    Add Loan view to router

--- public/main.js
+++ public/main.js
@@ -7,6 +7,7 @@ import Login from './views/Login.vue';
 import Portfolio from './views/Portfolio.vue';
 import Asset from './views/read/Asset.vue';
 import Deposit from './views/read/Deposit.vue';
+import Loan from './views/read/Loan.vue';
 
 Vue.use(VueRouter);
 
@@ -15,6 +16,7 @@ const routes = [
 	{ path: '/', component: Portfolio },
 	{ path: '/asset/:ticker', component: Asset },
 	{ path: '/deposit/:platform/:ticker', component: Deposit },
+	{ path: '/loan/:platform/:ticker', component: Loan },
 ];
 
 const router = new VueRouter({
