9b3c2dca35cbac99ae9f0b03fceef659b9808d83
Timur Badretdinov <destinerx@gmail.com>
2019-10-05 23:02:53 +0300

    NewLoan view

---
+++ public/views/write/NewLoan.vue
@@ -0,0 +1,324 @@
+<template>
+	<div>
+		<h2>Manage loans</h2>
+		<div>
+			<div class="list">
+				<div v-for="asset in assets" class="asset-selector" :class="{ 'selected': asset == assetTicker }" @click="selectAsset(asset)">
+					{{ asset }}
+				</div>
+			</div>
+		</div>
+		<div>
+			<div class="list">
+				<div v-for="platform in platforms" class="app-selector" :class="{ 'selected': platform == platformName }" @click="selectPlatform(platform)">
+					<div>{{ platform }}</div>
+					<div>{{ formatRate(rates[platform][assetTicker]) }}</div>
+				</div>
+			</div>
+		</div>
+		<div id="action-selector-wrapper">
+			<span class="action-selector" @click="selectAction('borrow')" :class="{ 'selected': action == 'borrow' }">Borrow</span>
+			<span class="action-selector" @click="selectAction('repay')" :class="{ 'selected': action == 'repay' }">Repay</span>
+		</div>
+		<div id="amount-wrapper">
+			<span class="input-group">
+				<span class="max-label" @click="setMax()">MAX</span>
+				<input class="amount" v-model="assetAmount">
+				<span class="label label-ghost label-right inline">{{ assetTicker }}</span>
+			</span>
+		</div>
+		<!-- <div style="margin-top: 2em">
+			<span class="badge badge-info">
+				You are depositing {{ assetAmount }} {{ assetTicker }} to {{ platformName }}.
+			</span>
+		</div> -->
+		<div style="margin-top: 1em">
+			<button class="primary big" @click="borrow()" v-if="action == 'borrow'">Borrow</button>
+			<button class="primary big" @click="repay()" v-if="action == 'withdraw'">Repay</button>
+		</div>
+		<TxStatus :status="txStatus" :onHidden="hideStatus"/>
+	</div>
+</template>
+
+<script>
+import Vue from 'vue';
+import BigNumber from 'bignumber.js';
+import { ethers } from 'ethers';
+
+import TxStatus from '../../components/TxStatus.vue';
+
+import erc20Abi from '../../data/abi/erc20.json';
+import compoundTokenAbi from '../../data/abi/compoundToken.json';
+import fulcrumTokenAbi from '../../data/abi/fulcrumToken.json';
+import decimals from '../../data/decimals.json';
+import addresses from '../../data/addresses.json';
+
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+const signer = provider.getSigner();
+
+export default {
+	components: {
+		TxStatus,
+	},
+	data() {
+		return {
+			account: undefined,
+			assetTicker: 'DAI',
+			assetAmount: '100',
+			platformName: 'Compound',
+			action: 'borrow',
+			rates: {
+				'Compound': {},
+				'Torque': {},
+			},
+			indices: {
+				'Compound': {},
+				'Torque': {},
+			},
+			tokenAddresses: {
+				'Compound': {},
+				'Torque': {},
+			},
+			txStatus: 'none',
+		}
+	},
+	mounted() {
+		const address = localStorage.getItem('address');
+		const auth = localStorage.getItem('auth') == 'true';
+		if (!address || !auth) {
+			this.$router.push('/login');
+			return;
+		}
+		this.account = {
+			address,
+			auth,
+		};
+		this.loadRates();
+	},
+	methods: {
+		selectAsset(asset) {
+			this.assetTicker = asset;
+		},
+		selectPlatform(platform) {
+			this.platformName = platform;
+		},
+		selectAction(action) {
+			this.action = action;
+		},
+		borrow() {
+			if (this.platformName == 'Compound') {
+				this.borrowCompound();
+			} else  {
+				this.borrowTorque();
+			}
+		},
+		repay() {
+		},
+		loadRates() {
+			this.loadCompoundRates();
+			this.loadTorqueRates();
+		},
+		async borrowCompound() {
+			const assetAddress = addresses[this.assetTicker];
+			const cTokenAddress = this.tokenAddresses['Compound'][this.assetTicker];
+			const cToken = new ethers.Contract(cTokenAddress, compoundTokenAbi, signer);
+			const borrowAmount = this.toLongAmount(this.assetAmount, this.assetTicker);
+			try {
+				this.txStatus = 'mining';
+				const tx = await cToken.borrow(borrowAmount);
+				const txReceipt = await provider.getTransactionReceipt(tx.hash);
+				if (txReceipt.status == 1) {
+					this.txStatus = 'success';
+				} else {
+					this.txStatus = 'failure';
+				}
+			} catch(e) {
+				this.txStatus = 'rejected';
+			}
+		},
+		async borrowTorque() {
+			const assetAddress = addresses[this.assetTicker];
+			const iTokenAddress = this.tokenAddresses['Torque'][this.assetTicker];
+			const iToken = new ethers.Contract(iTokenAddress, fulcrumTokenAbi, signer);
+			const account = this.account.address;
+			const borrowAmount = this.toLongAmount(this.assetAmount, this.assetTicker);
+			const leverageAmount = '4000000000000000000';
+			const initialLoanDuration = '7884000';
+			const collateral = addresses['WETH'];
+			const depositAmount = await iToken.getDepositAmountForBorrow(borrowAmount, leverageAmount, initialLoanDuration, collateral);
+			const depositAmountNumber = new BigNumber(depositAmount.toString());
+			const depositAmountWithInterest = depositAmountNumber.times(1.2).toFixed(0);
+			const valueNumber = new BigNumber(depositAmountWithInterest);
+			const ticker = this.assetTicker.toLowerCase();
+			const borrowEns = `${ticker}.tokenloan.eth`;
+			const borrowAddress = await provider.resolveName(borrowEns);
+			const borrowTx = {
+				value: '0x' + valueNumber.toString(16),
+				to: borrowAddress
+			};
+			try {
+				this.txStatus = 'mining';
+				const tx = await signer.sendTransaction(borrowTx);
+				const txReceipt = await provider.getTransactionReceipt(tx.hash);
+				if (txReceipt.status == 1) {
+					this.txStatus = 'success';
+				} else {
+					this.txStatus = 'failure';
+				}
+			} catch(e) {
+				this.txStatus = 'rejected';
+			}
+		},
+		async hideStatus() {
+			this.txStatus = 'none';
+		},
+		async checkAllowance(spender, address, amount) {
+			const uintMax = '115792089237316195423570985008687907853269984665640564039457584007913129639935';
+			const account = this.account.address;
+			const inputToken = new ethers.Contract(address, erc20Abi, signer);
+			const inputTokenAllowance = await inputToken.allowance(account, spender);
+			if (inputTokenAllowance.gte(amount)) {
+				return;
+			}
+			try {
+				this.txStatus = 'mining';
+				const tx = await inputToken.approve(spender, uintMax);
+				const txReceipt = await provider.getTransactionReceipt(tx.hash);
+				if (txReceipt.status == 1) {
+					this.txStatus = 'success';
+				} else {
+					this.txStatus = 'failure';
+				}
+			} catch(e) {
+				this.txStatus = 'rejected';
+			}
+		},
+		async loadCompoundRates() {
+			const url = "https://api.thegraph.com/subgraphs/name/destiner/compound";
+			const query = `
+				query {
+					tokens {
+						symbol
+						address
+						borrowRate
+						supplyIndex
+					}
+				}`;
+			const opts = {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({ query })
+			};
+			const response = await fetch(url, opts);
+			const json = await response.json();
+			const data = json.data;
+			const tokens = data.tokens;
+			for (const token of tokens) {
+				const ticker = token.symbol.substr(1);
+				const address = token.address;
+				const rawRate = token.borrowRate;
+				const index = token.supplyIndex;
+				const rawRateNumber = new BigNumber(rawRate);
+				const rateNumber = rawRateNumber.times('2102400').div('1e18');
+				const rate = rateNumber.toString();
+				Vue.set(this.tokenAddresses['Compound'], ticker, address);
+				Vue.set(this.rates['Compound'], ticker, rate);
+				Vue.set(this.indices['Compound'], ticker, index);
+			}
+		},
+		async loadTorqueRates() {
+			const url = "https://api.thegraph.com/subgraphs/name/destiner/fulcrum";
+			const query = `
+				query {
+					tokens {
+						symbol
+						address
+						index
+					}
+				}`;
+			const opts = {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({ query })
+			};
+			const response = await fetch(url, opts);
+			const json = await response.json();
+			const data = json.data;
+			const tokens = data.tokens;
+			for (const token of tokens) {
+				const ticker = token.symbol.substr(1);
+				const address = token.address;
+				const index = token.supplyIndex;
+				Vue.set(this.tokenAddresses['Torque'], ticker, address);
+				Vue.set(this.rates['Torque'], ticker, 0.16); // TODO
+				Vue.set(this.indices['Torque'], ticker, index);
+			}
+		},
+		toShortAmount(amount, ticker) {
+			const ten = new BigNumber(10);
+			const tickerDecimals = decimals[ticker];
+			const multiplier = ten.pow(tickerDecimals);
+			const amountNumber = new BigNumber(amount);
+			const shortAmountNumber = amountNumber.div(multiplier);
+			const shortAmount = shortAmountNumber.toString();
+			return shortAmount;
+		},
+		toLongAmount(amount, ticker) {
+			const ten = new BigNumber(10);
+			const tickerDecimals = decimals[ticker];
+			const multiplier = ten.pow(tickerDecimals);
+			const amountNumber = new BigNumber(amount);
+			const longAmountNumber = amountNumber.times(multiplier);
+			const longAmount = longAmountNumber.toFixed(0);
+			return longAmount;
+		},
+		async setMax() {
+			const account = this.account.address;
+			if (this.assetTicker == 'ETH') {
+				const etherBalance = await provider.getBalance(account);
+				const assetAmount = this.toShortAmount(etherBalance.toString(), this.assetTicker);
+				this.assetAmount = assetAmount;
+			} else {
+				const inputAddress = addresses[this.assetTicker];
+				const inputToken = new ethers.Contract(inputAddress, erc20Abi, provider);
+				const inputTokenBalance = await inputToken.balanceOf(account);
+				const assetAmount = this.toShortAmount(inputTokenBalance.toString(), this.assetTicker);
+				this.assetAmount = assetAmount;
+			}
+		},
+		formatRate(rateString) {
+			if (!rateString) {
+				return 'Loading…';
+			}
+			const rate = new BigNumber(rateString);
+			return `${rate.times(100).toFixed(2)}%`;
+		}
+	},
+	computed: {
+		assets() {
+			return [ 'DAI', 'USDC' ];
+		},
+		platforms() {
+			return [ 'Compound', 'Torque' ];
+		},
+	},
+}
+</script>
+
+<style scoped>
+#action-selector-wrapper {
+	margin-top: 0.5em;
+}
+
+#amount-wrapper {
+	margin-top: 3em;
+}
+
+.list {
+	display: flex;
+}
+
+.inline {
+	display: inline-block;
+}
+</style>
