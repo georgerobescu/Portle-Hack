9eda101c7f631a0ccecaa935c42827cccc9eecc4
Timur Badretdinov <destinerx@gmail.com>
2019-10-15 10:13:08 +0300

    Long leverage via Compound

---
+++ public/views/manage/advanced/Leverage.vue
@@ -0,0 +1,355 @@
+<template>
+	<div>
+		<div id="action-selector-wrapper">
+			<span class="action-selector" @click="selectAction('long')" :class="{ 'selected': action == 'long' }">Long</span>
+			<span class="action-selector" @click="selectAction('short')" :class="{ 'selected': action == 'short' }">Short</span>
+		</div>
+		<div id="picker-wrapper">
+			Long
+			<span class="input-group">
+				<Picker :value="targetAsset" :list="targetAssets" :onSelect="targetAssetSelected" class="inline"/>
+			</span>
+			against
+			<span class="input-group">
+				<Picker :value="loanAsset" :list="loanAssets" :onSelect="loanAssetSelected" class="inline"/>
+			</span>
+			using
+			<span class="input-group">
+				<Picker :value="platform" :list="platforms" :onSelect="platformSelected" class="inline"/>
+			</span>
+		</div>
+		<div id="rate-wrapper">
+			Leverage:
+			<span class="action-selector" @click="setRate('1.25')" :class="{ 'selected': rate == '1.25' }">1.25x</span>
+			<span class="action-selector" @click="setRate('1.5')" :class="{ 'selected': rate == '1.5' }">1.5x</span>
+			<span class="action-selector" @click="setRate('1.66')" :class="{ 'selected': rate == '1.66' }">1.66x</span>
+		</div>
+		<div id="amount-wrapper">
+			<span class="input-group">
+				<span class="label">Position</span>
+				<span class="max-label" @click="setMax()">MAX</span>
+				<input class="amount" v-model="amount">
+				<span class="label label-ghost label-right inline">{{ targetAsset }}</span>
+			</span>
+		</div>
+		<div id="button-wrapper">
+			<button class="primary big" @click="long()" v-if="action == 'long'">Long</button>
+			<button class="primary big" @click="short()" v-if="action == 'short'">Short</button>
+		</div>
+		<TxStatus :status="txStatus" :onHidden="hideStatus"/>
+	</div>
+</template>
+
+<script>
+import Vue from 'vue';
+import BigNumber from 'bignumber.js';
+import { ethers } from 'ethers';
+
+import Picker from '../../../components/Picker.vue';
+import TxStatus from '../../../components/TxStatus.vue';
+
+import erc20Abi from '../../../data/abi/erc20.json';
+import kyberProxyAbi from '../../../data/abi/kyberProxy.json';
+import compoundTokenAbi from '../../../data/abi/compoundToken.json';
+import fulcrumTokenAbi from '../../../data/abi/fulcrumToken.json';
+
+import prices from '../../../data/prices.json';
+import decimals from '../../../data/decimals.json';
+import addresses from '../../../data/addresses.json';
+
+const kyberProxyAddress = '0x818e6fecd516ecc3849daf6845e3ec868087b755';
+
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+const signer = provider.getSigner();
+
+export default {
+	components: {
+		Picker,
+		TxStatus,
+	},
+	data() {
+		return {
+			account: undefined,
+			txStatus: 'none',
+			action: 'long',
+			targetAsset: 'ETH',
+			loanAsset: 'DAI',
+			platform: 'Compound',
+			rate: '1.5',
+			amount: '10',
+			tokenAddresses: {
+				'Compound': {},
+				'Fulcrum': {},
+			},
+		}
+	},
+	mounted() {
+		this.loadAccount();
+		if (!this.account) {
+			this.$router.push('/login');
+			return;
+		}
+		this.loadCompound();
+		this.loadFulcrum();
+	},
+	methods: {
+		selectAction(action) {
+			this.action = action;
+		},
+		setRate(rate) {
+			this.rate = rate;
+		},
+		targetAssetSelected(ticker) {
+			this.targetAsset = ticker;
+		},
+		loanAssetSelected(ticker) {
+			this.loanAsset = ticker;
+		},
+		platformSelected(platform) {
+			this.platform = platform
+		},
+		async setMax() {
+			const account = this.account.address;
+			if (this.targetAsset == 'ETH') {
+				const etherBalance = await provider.getBalance(account);
+				const amount = this.toShortAmount(etherBalance.toString(), this.targetAsset);
+				this.amount = amount;
+			} else {
+				const inputAddress = this.getTokenAddress(this.targetAsset);
+				const inputToken = new ethers.Contract(inputAddress, erc20Abi, provider);
+				const inputTokenBalance = await inputToken.balanceOf(account);
+				const amount = this.toShortAmount(inputTokenBalance.toString(), this.targetAsset);
+				this.amount = amount;
+			}
+		},
+		async hideStatus() {
+			this.txStatus = 'none';
+		},
+		async long() {
+			await this.deposit();
+			await this.borrow();
+			await this.trade();
+		},
+		async short() {
+		},
+		loadAccount() {
+			const address = localStorage.getItem('address');
+			const auth = localStorage.getItem('auth') == 'true';
+			if (!address) {
+				return;
+			}
+			this.account = {
+				address,
+				auth,
+			};
+		},
+		async deposit() {
+			if (this.platform == 'Compound') {
+				await this.depositCompound();
+			}
+		},
+		async borrow() {
+			if (this.platform == 'Compound') {
+				await this.borrowCompound();
+			}
+		},
+		async trade() {
+			await this.tradeKyber();
+		},
+		async depositCompound() {
+			const assetAddress = addresses[this.targetAsset];
+			const cTokenAddress = this.tokenAddresses['Compound'][this.targetAsset];
+			const cToken = new ethers.Contract(cTokenAddress, compoundTokenAbi, signer);
+			const positionAmountNumber = new BigNumber(this.amount);
+			const depositAmountNumber = positionAmountNumber.div(this.rate);
+			const depositAmount = this.toLongAmount(depositAmountNumber, this.targetAsset);
+			await this.checkAllowance(cTokenAddress, assetAddress, depositAmount);
+			try {
+				this.txStatus = 'mining';
+				const tx = await cToken.mint(depositAmount);
+				const txReceipt = await provider.getTransactionReceipt(tx.hash);
+				if (txReceipt.status == 1) {
+					this.txStatus = 'success';
+				} else {
+					this.txStatus = 'failure';
+				}
+			} catch(e) {
+				this.txStatus = 'rejected';
+			}
+		},
+		async borrowCompound() {
+			const assetAddress = addresses[this.loanAsset];
+			const cTokenAddress = this.tokenAddresses['Compound'][this.loanAsset];
+			const cToken = new ethers.Contract(cTokenAddress, compoundTokenAbi, signer);
+			const positionAmountNumber = new BigNumber(this.amount);
+			const depositAmountNumber = positionAmountNumber.div(this.rate);
+			const borrowTargetAmountNumber = positionAmountNumber.minus(depositAmountNumber);
+			const borrowAmountNumber = borrowTargetAmountNumber.times(prices[this.targetAsset]).div(prices[this.loanAsset]);
+			const borrowAmount = this.toLongAmount(borrowAmountNumber, this.loanAsset);
+			try {
+				this.txStatus = 'mining';
+				const tx = await cToken.borrow(borrowAmount);
+				const txReceipt = await provider.getTransactionReceipt(tx.hash);
+				if (txReceipt.status == 1) {
+					this.txStatus = 'success';
+				} else {
+					this.txStatus = 'failure';
+				}
+			} catch(e) {
+				this.txStatus = 'rejected';
+			}
+		},
+		async tradeKyber() {
+			const assetAddress = addresses[this.loanAsset];
+			const positionAmountNumber = new BigNumber(this.amount);
+			const depositAmountNumber = positionAmountNumber.div(this.rate);
+			const borrowTargetAmountNumber = positionAmountNumber.minus(depositAmountNumber);
+			const borrowAmountNumber = borrowTargetAmountNumber.times(prices[this.targetAsset]).div(prices[this.loanAsset]);
+			const minConversionRate = '1';
+			const kyberProxy = new ethers.Contract(kyberProxyAddress, kyberProxyAbi, signer);
+			if (this.targetAsset == 'ETH') {
+				// Token to eth
+				const inputAddress = this.getTokenAddress(this.loanAsset);
+				const inputAmount = this.toLongAmount(borrowAmountNumber, this.loanAsset);
+				await this.checkAllowance(kyberProxyAddress, inputAddress, inputAmount);
+				try {
+					this.txStatus = 'mining';
+					const tx = await kyberProxy.swapTokenToEther(inputAddress, inputAmount, minConversionRate);
+					const txReceipt = await provider.getTransactionReceipt(tx.hash);
+					if (txReceipt.status == 1) {
+						this.txStatus = 'success';
+					} else {
+						this.txStatus = 'failure';
+					}
+				} catch(e) {
+					this.txStatus = 'rejected';
+				}
+			} else {
+				// Token to token
+				const inputAddress = this.getTokenAddress(this.loanAsset);
+				const inputAmount = this.toLongAmount(borrowAmountNumber, this.loanAsset);
+				const outputAddress = this.getTokenAddress(this.targetAsset);
+				await this.checkAllowance(kyberProxyAddress, inputAddress, inputAmount);
+				try {
+					this.txStatus = 'mining';
+					const tx = await kyberProxy.swapTokenToToken(inputAddress, inputAmount, outputAddress, minConversionRate);
+					const txReceipt = await provider.getTransactionReceipt(tx.hash);
+					if (txReceipt.status == 1) {
+						this.txStatus = 'success';
+					} else {
+						this.txStatus = 'failure';
+					}
+				} catch(e) {
+					this.txStatus = 'rejected';
+				}
+			}
+		},
+		async loadCompound() {
+			const url = "https://api.thegraph.com/subgraphs/name/destiner/compound";
+			const query = `
+				query {
+					tokens {
+						symbol
+						address
+					}
+				}`;
+			const opts = {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({ query })
+			};
+			const response = await fetch(url, opts);
+			const json = await response.json();
+			const data = json.data;
+			const tokens = data.tokens;
+			for (const token of tokens) {
+				const ticker = token.symbol.substr(1);
+				const address = token.address;
+				Vue.set(this.tokenAddresses['Compound'], ticker, address);
+			}
+		},
+		async loadFulcrum() {
+			const url = "https://api.thegraph.com/subgraphs/name/destiner/fulcrum";
+			const query = `
+				query {
+					tokens {
+						symbol
+						address
+					}
+				}`;
+			const opts = {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({ query })
+			};
+			const response = await fetch(url, opts);
+			const json = await response.json();
+			const data = json.data;
+			const tokens = data.tokens;
+			for (const token of tokens) {
+				const ticker = token.symbol.substr(1);
+				const address = token.address;
+				Vue.set(this.tokenAddresses['Fulcrum'], ticker, address);
+			}
+		},
+		toShortAmount(amount, ticker) {
+			const ten = new BigNumber(10);
+			const tickerDecimals = decimals[ticker];
+			const multiplier = ten.pow(tickerDecimals);
+			const amountNumber = new BigNumber(amount);
+			const shortAmountNumber = amountNumber.div(multiplier);
+			const shortAmount = shortAmountNumber.toString();
+			return shortAmount;
+		},
+		toLongAmount(amount, ticker) {
+			const ten = new BigNumber(10);
+			const tickerDecimals = decimals[ticker];
+			const multiplier = ten.pow(tickerDecimals);
+			const amountNumber = new BigNumber(amount);
+			const longAmountNumber = amountNumber.times(multiplier);
+			const longAmount = longAmountNumber.toFixed(0);
+			return longAmount;
+		},
+		getTokenAddress(ticker) {
+			if (ticker == 'ETH') {
+				return '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE';
+			}
+			return addresses[ticker];
+		},
+	},
+	computed: {
+		targetAssets() {
+			return ['ETH', 'ZRX'];
+		},
+		loanAssets() {
+			return ['DAI', 'USDC'];
+		},
+		platforms() {
+			return ['Compound', 'Fulcrum'];
+		},
+	},
+}
+</script>
+
+<style scoped>
+#action-selector-wrapper {
+	margin-top: 1em;
+}
+
+#picker-wrapper {
+	margin-top: 3em;
+}
+
+#rate-wrapper {
+	margin-top: 1em;
+}
+
+#amount-wrapper {
+	margin-top: 3em;
+}
+
+#button-wrapper {
+	margin-top: 1em;
+}
+
+</style>
